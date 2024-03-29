88ef57c40fea3fa2b3cc85751805655a9afe8fd7
azharmca08 <azhar.mca08@gmail.com>
2024-03-30 03:02:37 +0530

    File updated

--- limit-service/src/main/java/com/project/limitservice/LimitServiceApplication.java
+++ limit-service/src/main/java/com/project/limitservice/LimitServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LimitServiceApplication {
-	//my updated file
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LimitServiceApplication.class, args);
 	}
